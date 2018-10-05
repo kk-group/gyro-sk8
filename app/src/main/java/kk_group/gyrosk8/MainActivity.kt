d9c45aae3e866e8abc195e449f7c59d36a51acf6
taakib <piratoz22@gmail.com>
2018-10-05 15:29:00 +0300

    IntroActivity, PrefManager, MainActivity + XML files

--- app/src/main/java/kk_group/gyrosk8/MainActivity.kt
+++ app/src/main/java/kk_group/gyrosk8/MainActivity.kt
@@ -1,15 +1,29 @@
 package kk_group.gyrosk8
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().add(R.id.root_layout, SandboxFragment()).commit()
+        btShowSlider.setOnClickListener {
+            PrefManager(applicationContext).setLaunched(true)
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }
+
+        btShowSlider2.setOnClickListener {
+            supportFragmentManager.beginTransaction().add(R.id.root_layout, SandboxFragment()).commit()
+            btShowSlider.visibility = View.GONE
+            btShowSlider2.visibility = View.GONE
+        }
 
     }
 }
